217057f5dc016b9d143e62aaea29ffb41c9b8a00
subodhatwork <subodhkamble569@gmail.com>
2025-12-05 21:47:07 +0530

    Add files via upload

---
+++ banking system .py
@@ -0,0 +1,38 @@
+# banking system project.
+print("Welcome To Our Bank.")
+name=input("Plese Enter Your Name.......")
+age=input("Enter Your Current Age")
+print("We Offer Various Services.\n(Press 1 For) Account Related\n(Press 2 For) Loan related\n(Press 3 For) UPI Related\n(Press 4 For Others)")
+services= int(input("Which Service Are You Intrested in ?"))
+
+if services==1:
+   1==print(int(input("Press 1 For A/c Oppening\nPress 2 For A/C Clousure.")))
+   if 1==1:
+      print("Visit..............")
+   else:
+      print("visit,,,,,,,,,,,,,,,")
+elif services==2:
+   2==print(int(input("Press 1 For Home Loan\nPress 2 For Car Loan\nPress 3 For other Loans.")))
+   if 2==1:
+      print("Check Eligiblity Here..........")
+   elif 2==2:
+      print("Check Eligeblity Here...........")
+   else:
+      print("Visit To The Bank")
+elif services==3:
+   3==print(int(input("Press 1 For Reporting Any Isue\nPress 2 for UPI Satement\nPress 3 For Deleting UPI Id\nPress 4 For Other")))
+   if 3==1:
+      print("Report At ...........................")
+   elif 3==2:
+      print("Download Statement From banana.com")
+   elif 3==3:
+      print("Click Here For Proceeding")    
+   else:
+      print("Visit The Bank")
+else:
+   print("If Yor Are Just Wasting Our Time Plese Dont\nIf Legit Then Visit The Bank.")
+
+        
+
+
+      
